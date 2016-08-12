9707cc01906d67b2d436efd65ca4ffd37ce34073
user1 <mail@email.com>
2016-08-11 20:58:05 -0500

    Initial commit

---
+++ src/main/java/org/test/PropertyPrinter.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class PropertyPrinter {
+	private String property;
+
+	public String print(){
+		return this.property;
+	}
+	
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+}
